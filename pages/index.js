d3a095cddb1541f121fc805e591dc4e24483a94d
Parker Stafford <parker.stafford92@gmail.com>
2021-02-17 10:45:41 -0800

    Add  links

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <Head>
-      <title>Parker Stafford</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <>
+      <Head>
+        <title>Parker Stafford</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hello</h1>
+      <h3>I&apos;m Parker!</h3>
+      <p>I&apos;m a software engineer in Washington State.</p>
+      <Link href="/resume.pdf">Resume</Link>
+      <Link href="/blog">Blog</Link>
+      <Link href="about-me">About me</Link>
+      <Link href="work">Selected work</Link>
+    </>
   );
 }
